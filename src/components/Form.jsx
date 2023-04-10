ffbc41a024b84b0c8a67380d4f7f418320704fcc
Gabriel Vinicius <gabrielviniciusdesouzaoliveira@gmail.com>
2023-04-10 13:37:06 +0100

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	new file:   src/components/EditTodoDialog.jsx 	new file:   src/components/Form.jsx 	new file:   src/components/TodoItem.jsx 	new file:   src/components/style.css 	new file:   src/pages/Home.jsx 	new file:   src/pages/style.css

---
+++ src/components/Form.jsx
@@ -0,0 +1,37 @@
+import { Button, Paper, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+export default function Form({ addTodo }) {
+  const [text, setText] = useState(null);
+  const [id, setId] = useState(0);
+
+  const todoCreate = (text) => {
+    if (!text || text.trim() === '') {
+     return(
+      window.alert("Erro: Add a texto to keep")
+     );
+    }
+    const todoObj = { text: text, id: id };
+    setId(id + 1);
+    addTodo(todoObj);
+    document.getElementById("outlined-basic").value = null;
+   
+  };
+
+  return (
+    <Paper style={{ padding: "1em" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <TextField
+          id="outlined-basic"
+          label="Home Tasks"
+          variant="outlined"
+          onChange={(e) => setText(e.target.value)}
+          fullWidth
+        />
+        <Button variant="text" onClick={() => todoCreate(text)}>
+          Add
+        </Button>
+      </div>
+    </Paper>
+  );
+}
